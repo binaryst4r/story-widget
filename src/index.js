f19cc722c005c6c4db19429aed3d7d7debfa4d7b
Larry Gust <larry.v.gust@gmail.com>
2021-10-15 22:05:44 -0500

    ayyyy its a widget

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const WidgetNodes = document.querySelectorAll('.story-widget')
+
+WidgetNodes.forEach(node => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    node
+  );
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
